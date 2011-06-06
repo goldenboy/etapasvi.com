72e1400dd2444a0bc149f798be55b3a445b59872
saynt2day <saynt2day@gmail.com>
2011-06-06 08:13:09 -0700

    errors

--- lib/symfony/exception/data/error.html.php
+++ lib/symfony/exception/data/error.html.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="title" content="eTapasvi.com" />
+<title>eTapasvi.com</title>
+<link rel="Shortcut Icon" type="image/x-icon" href="http://www.etapasvi.com/favicon.ico" />
+</head>
+
+<body>
 <?php
 /*
 setcookie( 'phpbb3_nqoeg_sid', "", time() - 3600, '/');
@@ -7,17 +17,19 @@ setcookie( 'culture', "", time() - 3600, '/');
 setcookie( 'sdfgjlk', "", time() - 3600, '/');
 setcookie( 'symfony', "", time() - 3600, '/');*/
 ?>
-
 <center>
+<a href="http://www.etapasvi.com" ><img src="http://www.etapasvi.com/i/bg_circle.jpg" style="border:0" alt="eTapasvi.com"/></a>
+<p style="font: 0.7em/15px Verdana;">
+    <strong><a href="http://www.etapasvi.com" >www.eTapasvi.com</a> is temporarily unavailable.</strong>
+    <br/>We apologize for any inconvenience this causes, and thank you for your patience during this time.
+    <br/>Contact e-mail: <a href="mailto:etapasvi@gmail.com">etapasvi@gmail.com</a>
+</p>
 
-<a href="http://www.etapasvi.com" ><img src="http://www.etapasvi.com/i/bg_circle.jpg" style="border:0"/></a>
-
-<h5 style="color:#555753">
-<a href="http://www.etapasvi.com" >www.eTapasvi.com</a> is temporarily unavailable. Contact e-mail: <a href="mailto:etapasvi@gmail.com">etapasvi@gmail.com</a> <br/>We apologize for any inconvenience this causes, and thank you for your patience during this time.
-</h5>
-
-<h5 style="color:#555753">
-Сайт <a href="http://www.etapasvi.com" >www.eTapasvi.com</a> временно недоступен. Ведутся технические работы. E-mail для связи: <a href="mailto:etapasvi@gmail.com">etapasvi@gmail.com</a> <br/>Приносим извинения за причинённые неудобства и благодарим за терпение.
-</h5>
-
+<p style="font: 0.7em/15px Verdana;">
+    <strong>Сайт <a href="http://www.etapasvi.com" >www.eTapasvi.com</a> временно недоступен. Ведутся технические работы. </strong>
+    <br/>Приносим извинения за причинённые неудобства и благодарим за терпение.
+    <br/>E-mail для связи: <a href="mailto:etapasvi@gmail.com">etapasvi@gmail.com</a> 
+</p>
 </center>
+</body>
+</html>
