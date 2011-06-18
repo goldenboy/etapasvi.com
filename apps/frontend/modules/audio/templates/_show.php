58a1a58700424c87841724cbf695bab1c92dcab2
saynt2day <saynt2day@gmail.com>
2011-06-18 10:15:26 -0700

    audio show

--- apps/frontend/modules/audio/templates/_show.php
+++ apps/frontend/modules/audio/templates/_show.php
@@ -40,7 +40,7 @@ $author = $audio->getAuthor($sf_user->getCulture(), true);
 </div>
 
 <object type="application/x-shockwave-flash" data="http://kiwi6.com/swf/player.swf" class="audioplayer" height="22" width="290" allowscriptaccess="always">
-<param name="movie" value="http://kiwi6.com/swf/player.swf" /><param name="FlashVars" value="playerID=audioplayer&soundFile=<?php echo $audio->getDirectUrl(); ?>" />
+<param name="movie" value="http://kiwi6.com/swf/player.swf" /><param name="FlashVars" value="playerID=audioplayer&amp;soundFile=<?php echo $audio->getDirectUrl(); ?>" />
 <param name="quality" value="high" /><param name="menu" value="false" /><param name="allowscriptaccess" value="always" /><param name="wmode" value="transparent" /></object>
 
 </div> 
