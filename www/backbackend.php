1de179cf1e2eb8b0ca8107db3a6ff530c1dd0833
saynt2day <saynt2day@gmail.com>
2011-06-19 09:38:18 -0700

    backend hide warnings

--- www/backbackend.php
+++ www/backbackend.php
@@ -2,10 +2,10 @@
 
 //phpinfo();
 //exit();
-
+/*
 ini_set( 'error_reporting', 'E_ALL' );
 ini_set( 'display_errors', 'on' );
-error_reporting(E_ALL);
+error_reporting(E_ALL);*/
 require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
 
 $configuration = ProjectConfiguration::getApplicationConfiguration('backend', 'prod', false);
