8c1d5fa17666e24b2f9e2dead4b2e9d55ae6d50f
saynt2day <saynt2day@gmail.com>
2011-07-27 23:51:52 -0700

    #26 chat

---
+++ apps/frontend/modules/chat/templates/showSuccess.php
@@ -0,0 +1,4 @@
+<?php slot('body_id') ?>body_chat<?php end_slot() ?>
+<h1><?php echo __('Chat') ?></h1>
+
+<object width="100%" height="454" ><param name="movie" value="http://widget.meebo.com/mm.swf?tCBlHHYXxA"/><embed src="http://widget.meebo.com/mm.swf?tCBlHHYXxA" type="application/x-shockwave-flash" width="100%" height="454"></embed></object>
