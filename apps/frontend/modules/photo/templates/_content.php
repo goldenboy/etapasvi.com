e0a7458d34e63301136e33868a0833cc04754e99
saynt2day <saynt2day@gmail.com>
2011-08-18 14:20:45 -0700

    #97 photo comments disqus_url

--- apps/frontend/modules/photo/templates/_content.php
+++ apps/frontend/modules/photo/templates/_content.php
@@ -8,7 +8,7 @@ if (!empty($photo)) {
 
 <?php include_component('photo', 'showwrapper', array('id'=>$id, 'title'=>$title, 'photo'=>$photo)); ?>
 <?php include_partial('comments/tools', array('empty'=>true)); ?>
-<?php include_component('comments', 'show', array('for'=>strtolower(ItemtypesPeer::ITEM_TYPE_NAME_PHOTO), 'id'=>$id, 'culture'=>$sf_user->getCulture(), 'comments_page_url'=>$$comments_page_url)) ?>	
+<?php include_component('comments', 'show', array('for'=>strtolower(ItemtypesPeer::ITEM_TYPE_NAME_PHOTO), 'id'=>$id, 'culture'=>$sf_user->getCulture(), 'comments_page_url'=>$comments_page_url)) ?>	
 <script type="text/javascript">
     preparePhotoContent();
 </script>
