46798b46b1ac8bdb5402c0f12586bf02a2d4ce1d
saynt2day <saynt2day@gmail.com>
2011-08-19 14:06:46 -0700

    #112 rss to main

--- apps/frontend/modules/news/templates/mainSuccess.php
+++ apps/frontend/modules/news/templates/mainSuccess.php
@@ -28,9 +28,12 @@
 
 <h2><?php echo __('Subscribe to News') ?></h2>
 <form action="http://feedburner.google.com/fb/a/mailverify" method="post" target="popupwindow" onsubmit="window.open('http://feedburner.google.com/fb/a/mailverify?uri=<?php echo $sf_user->getCulture(); ?>/etapasvi', 'popupwindow', 'scrollbars=yes,width=550,height=520');return true">
+<a href="http://feeds.feedburner.com/<?php echo $sf_user->getCulture(); ?>/etapasvi" class="right"><img src="http://feeds.feedburner.com/~fc/<?php echo $sf_user->getCulture(); ?>/etapasvi?bg=d4d0c8&amp;fg=444444&amp;anim=0" height="26" width="88" alt="RSS" /></a>
 E-mail: <input type="text" style="width:140px" name="email"/> &nbsp;<input type="hidden" value="<?php echo $sf_user->getCulture(); ?>/etapasvi" name="uri"/><input type="hidden" name="loc" value="<?php echo UserPeer::getCultureFeedburderLoc(); ?>"/><input type="submit" class="input_button" value="<?php echo __('Subscribe') ?>" />
+
 </form>
 <br/>
+<hr class="dashed"/>
 <h2><?php echo __('Comments') ?></h2>
 <?php include_component('comments', 'show') ?>
 
