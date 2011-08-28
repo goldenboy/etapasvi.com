99e1c8cbfa82f4e9e505edbc69fdadb7618f4cf1
saynt2day <saynt2day@gmail.com>
2011-08-28 10:26:29 -0700

    #129 video subtitles

--- apps/frontend/modules/video/templates/_embed.mobile.php
+++ apps/frontend/modules/video/templates/_embed.mobile.php
@@ -1,7 +1,7 @@
 <?php if (!empty($code)): ?>
     <?php if (strlen($code) <20): ?>
     <?php /* YouTube */ ?>
-    <iframe height="349" frameborder="0" width="560" allowfullscreen="" src="http://www.youtube.com/embed/<?php echo $code; ?>"></iframe></br>
+    <iframe height="349" frameborder="0" width="560" allowfullscreen="" src="http://www.youtube.com/embed/<?php echo $code; ?>?cc_load_policy=1&amp;hl=<?php echo UserPeer::getCultureIso($sf_user->getCulture()); ?>&amp;version=3"></iframe></br>
     <a href="http://www.youtube.com/watch?v=<?php echo $code; ?>"><?php echo __('Watch on YouTube'); ?></a>
     <?php else: ?>
         <?php echo html_entity_decode($code); ?>
