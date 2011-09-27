77b6bf53695802ce1815fc528807e4bb7ae57a22
saynt2day <saynt2day@gmail.com>
2011-09-27 09:09:23 -0700

    forum in menu

--- apps/frontend/templates/_menu.php
+++ apps/frontend/templates/_menu.php
@@ -60,6 +60,7 @@
 	<ul>*/?>
 		<?php /*<li><a href="<?php echo url_for('@user_member', true); ?>" title="<?php echo __('Member') ?>" id="mi_member"><?php echo __('Member') ?></a></li> */?>        
 		<li id="mi_chat"><a href="<?php echo url_for('@chat', true); ?>" title="<?php echo __('Chat') ?>"><?php echo __('Chat') ?></a></li>
+		<li id="mi_forum"><a href="http://forum.etapasvi.com" title="<?php echo __('Forum') ?>"><?php echo __('Forum') ?></a></li>
 		<li id="mi_social_tools"><a href="<?php echo url_for('@social_tools', true); ?>" title="<?php echo __('Social Tools') ?>"><?php echo __('Social Tools') ?></a></li>        
 		<li id="mi_search"><a href="<?php echo url_for('@search', true); ?>" title="<?php echo __('Search') ?>"><?php echo __('Search') ?></a></li>
         <li id="mi_servers"><a href="<?php echo url_for('@servers', true); ?>" title="<?php echo __('Servers') ?>"><?php echo __('Servers') ?></a></li>
