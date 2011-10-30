99e7918a82149d348b55b8070fcfbb56732b2ae8
saynt2day <saynt2day@gmail.com>
2011-10-30 06:04:30 -0700

    #238 web_directory fix

--- www/frontend_dev_www_etapasvi_com.php
+++ www/frontend_dev_www_etapasvi_com.php
@@ -26,6 +26,22 @@ if (!in_array(@$_SERVER['REMOTE_ADDR'], array('94.241.41.3', '::1')))
   die('You are not allowed to access this file.');
 }*/
 
+// Если на сервере веб-директория не /www/, заменяем:
+//    [DOCUMENT_ROOT] => /home8/etapasvi/public_html
+//    [REQUEST_URI] => /t.php
+//    [SCRIPT_NAME] => /www/t.php
+//    [PHP_SELF] => /www/t.php
+// на
+//    [DOCUMENT_ROOT] => /home/saynt2day20/etapasvi.com/www
+//    [REQUEST_URI] => /t.php
+//    [SCRIPT_NAME] => /t.php
+//    [PHP_SELF] => /t.php
+if (preg_match("/^\/www\/.*/", $_SERVER['SCRIPT_NAME'])) {
+	$_SERVER['DOCUMENT_ROOT'] .= '/www';
+	$_SERVER['SCRIPT_NAME']   = preg_replace("/^\/www/", '', $_SERVER['SCRIPT_NAME']);
+	$_SERVER['PHP_SELF']      = preg_replace("/^\/www/", '', $_SERVER['PHP_SELF']);
+}
+
 require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
 
 $configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'dev', true);
