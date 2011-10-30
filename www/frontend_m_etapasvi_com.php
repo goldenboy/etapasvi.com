525083a54d5749b766e6af6d2150a6861bf8c478
saynt2day <saynt2day@gmail.com>
2011-10-30 05:46:22 -0700

    #238 web_directory fix

--- www/frontend_m_etapasvi_com.php
+++ www/frontend_m_etapasvi_com.php
@@ -10,6 +10,22 @@ $_SERVER['HTTP_HOST']        = 'm.etapasvi.com';
 // http://tasks.etapasvi.com/issues/242
 $_SERVER['HTTP_X_FORWARDED_HOST']   = $_SERVER['HTTP_HOST'];
 
+// Если на сервере веб-директория не /www/, заменяем:
+//    [DOCUMENT_ROOT] => /home8/etapasvi/public_html
+//    [REQUEST_URI] => /t.php
+//    [SCRIPT_NAME] => /www/t.php
+//    [PHP_SELF] => /www/t.php
+// на
+//    [DOCUMENT_ROOT] => /home/saynt2day20/etapasvi.com/www
+//    [REQUEST_URI] => /t.php
+//    [SCRIPT_NAME] => /t.php
+//    [PHP_SELF] => /t.php
+if (preg_match("/^\/www\/.*/", $_SERVER['SCRIPT_NAME'])) {
+	$_SERVER['DOCUMENT_ROOT'] .= '/www';
+	$_SERVER['SCRIPT_NAME']   = preg_replace("/^\/www/", '', $_SERVER['SCRIPT_NAME']);
+	$_SERVER['PHP_SELF']      = preg_replace("/^\/www/", '', $_SERVER['PHP_SELF']);
+}
+
 // если к скрипту обратились из консоли, подменяем пермененные в $_SERVER
 if (!empty($_SERVER['SHELL']) || empty($_SERVER['DOCUMENT_ROOT'])) {
 	
