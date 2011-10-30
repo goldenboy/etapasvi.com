41432cd0b75e0754bba49b08d5595d83c892e39c
saynt2day <saynt2day@gmail.com>
2011-10-30 06:20:47 -0700

    #238 web_directory fix

--- www/backbackend.php
+++ www/backbackend.php
@@ -1,5 +1,21 @@
 <?php
 
+// Если на сервере веб-директория не /www/, заменяем:
+//    [DOCUMENT_ROOT] => /home8/etapasvi/public_html
+//    [REQUEST_URI] => /t.php
+//    [SCRIPT_NAME] => /www/t.php
+//    [PHP_SELF] => /www/t.php
+// на
+//    [DOCUMENT_ROOT] => /home/saynt2day20/etapasvi.com/www
+//    [REQUEST_URI] => /t.php
+//    [SCRIPT_NAME] => /t.php
+//    [PHP_SELF] => /t.php
+if (preg_match("/^\/www\/.*/", $_SERVER['SCRIPT_NAME'])) {
+	$_SERVER['DOCUMENT_ROOT'] .= '/www';
+	$_SERVER['SCRIPT_NAME']   = preg_replace("/^\/www/", '', $_SERVER['SCRIPT_NAME']);
+	$_SERVER['PHP_SELF']      = preg_replace("/^\/www/", '', $_SERVER['PHP_SELF']);
+}
+
 //phpinfo();
 //exit();
 /*
