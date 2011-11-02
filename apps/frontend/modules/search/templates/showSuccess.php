4982bc6cb5e8c70bd4ab3d8abcdcb4cf2e021986
saynt2day <saynt2day@gmail.com>
2011-11-02 10:55:03 -0700

    #265 adsense

--- apps/frontend/modules/search/templates/showSuccess.php
+++ apps/frontend/modules/search/templates/showSuccess.php
@@ -4,12 +4,29 @@
 
 <!-- Google Custom Search Element -->
 <div id="cse" style="width:100%;">Loading</div>
-<script src="http://www.google.com/jsapi" type="text/javascript"></script>
-<script type="text/javascript">
+<script src="//www.google.ru/jsapi" type="text/javascript"></script>
+<script type="text/javascript"> 
+  var _gaq = _gaq || [];
+  _gaq.push(["_setAccount", "UA-4047144-3"]);
+  function _trackQuery(control, searcher, query) {
+    var loc = document.location;
+    var url = [
+      loc.pathname,
+      loc.search,
+      loc.search ? '&' : '?',
+      encodeURIComponent('q'),
+      '=',
+      encodeURIComponent(query)
+    ];
+    _gaq.push(["_trackPageview", url.join('')]);
+  }
+
   google.load('search', '1');
-  google.setOnLoadCallback(function(){
-    var cse = new google.search.CustomSearchControl();
-    cse.enableAds('pub-7066467414020246');
-    cse.draw('cse');
+  google.setOnLoadCallback(function() {
+    var customSearchControl = new google.search.CustomSearchControl('006414956786193237693:ixwluctz1yu');
+    customSearchControl.setResultSetSize(google.search.Search.FILTERED_CSE_RESULTSET);
+    customSearchControl.setSearchStartingCallback(null, _trackQuery);
+    customSearchControl.draw('cse');
   }, true);
 </script>
+<link rel="stylesheet" href="//www.google.com/cse/style/look/default.css" type="text/css" />
