470f557e70c0ad7781148a2f199b5fea8f4799c2
saynt2day <saynt2day@gmail.com>
2012-01-22 06:30:29 -0800

    #357 main header style

--- apps/frontend/modules/news/templates/mainSuccess.mobile.php
+++ apps/frontend/modules/news/templates/mainSuccess.mobile.php
@@ -1,6 +1,6 @@
 <?php slot('body_id') ?>body_main<?php end_slot() ?>
 
-<h1><?php echo __('A message of peace to our world today') ?></h1>
+<h1><?php echo __('A message of peace, an appeal to the world') ?></h1>
 
 <div id="menu">
 <?php include_partial('global/menu', array('body_id'=>$body_id /*, 'is_logged_in'=>UserPeer::authIsLoggedIn()*/) ); ?>	
