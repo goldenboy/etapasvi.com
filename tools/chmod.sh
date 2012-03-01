fc40db825d1daea5d65bf6565f16d09d17a2bfae
saynt2day <saynt2day@gmail.com>
2012-03-01 04:41:49 -0800

    #393 github

---
+++ tools/chmod.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+# Установка разрешений на файлы и директории
+
+SCRIPT_DIR=`which $0 | xargs dirname`
+# внимание: может содержать относительный путь
+PROJECT_DIR="${SCRIPT_DIR}/../"
+
+
+# на папки
+#chmod -R 775 ${PROJECT_DIR}
+find ${PROJECT_DIR} -type d -exec chmod 775 {} \;
+
+# на файлы
+find ${PROJECT_DIR} -type f -exec chmod 644 {} \;
+
+# конфиги
+chmod -R 640 ${PROJECT_DIR}config/*
+
+# GIT
+find ${PROJECT_DIR}.git/ -type f -exec chmod 664 {} \;
+find ${PROJECT_DIR}.git/objects/ -type f -exec chmod 444 {} \;
+
+# кэш страниц
+find ${PROJECT_DIR}www/cache/ -type f -exec chmod 666 {} \;
+
+# .sh скрипты
+find ${PROJECT_DIR} -type f -name '*.sh' -exec chmod 755 {} \;
+chmod 755 ${PROJECT_DIR}symfony
+chmod 755 ${PROJECT_DIR}tools/log_monitor
+
+# файлы логов
+# cache
+# проставляет 777
+#cd ${PROJECT_DIR}
+#./symfony project:permissions
+#chmod 755 symfony
+
+# логи
+find ${PROJECT_DIR}log -type d -exec chmod 777 {} \;
+find ${PROJECT_DIR}log -type f -exec chmod 666 {} \;
+
+# кэш симфони
+find ${PROJECT_DIR}cache -type d -exec chmod 777 {} \;
+find ${PROJECT_DIR}cache -type f -exec chmod 666 {} \;
