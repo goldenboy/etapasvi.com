bfb2e96e850f00f966e3335e9c7f6688d3871477
saynt2day <saynt2day@gmail.com>
2012-05-03 06:38:16 -0700

    #443 Video captions language

--- apps/frontend/modules/video/templates/_code.php
+++ apps/frontend/modules/video/templates/_code.php
@@ -1,6 +1,6 @@
 <?php if (!empty($code)): ?>
     <?php if (strlen($code) <20): ?>
-    <iframe height="382" frameborder="0" width="520" allowfullscreen="" src="http://www.youtube.com/embed/<?php echo $code; ?>?cc_load_policy=1&amp;hl=<?php echo UserPeer::getCultureMsdn(); ?>&wmode=opaque"></iframe>
+    <iframe height="382" frameborder="0" width="520" allowfullscreen="" src="http://www.youtube.com/embed/<?php echo $code; ?>?cc_load_policy=1&amp;hl=<?php echo UserPeer::getCultureMsdn(); ?>&wmode=opaque&cc_lang_pref=<?php echo UserPeer::getCultureMsdn(); ?>"></iframe>
     <?php else: ?>
         <?php echo html_entity_decode($code); ?>
     <?php endif ?>
