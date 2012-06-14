4bd99c3ca9ccd6351221df1904de34c148bf94fd
saynt2day <saynt2day@gmail.com>
2012-06-14 11:06:26 -0700

    470 Remove list button from toolbar

--- www/js/ckeditor/config.js
+++ www/js/ckeditor/config.js
@@ -14,12 +14,12 @@ CKEDITOR.editorConfig = function( config )
     config.toolbar_Custom =
     [
         { name: 'clipboard', items : [ 'Cut','Copy','Paste','PasteText','PasteFromWord','-','Undo','Redo' ] },
-        { name: 'paragraph', items : [ 'NumberedList','BulletedList','-','Outdent','Indent','-','Blockquote',
-        '-','JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock','-','BidiLtr','BidiRtl' ] },
+        { name: 'paragraph', items : [ 'JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock','-','BidiLtr','BidiRtl' ] },
+        { name: 'tools', items : [ 'About' ] },
+        '/',
         { name: 'styles', items : [ 'Font','FontSize' ] },
         { name: 'basicstyles', items : [ 'Bold','Italic','Underline','Strike','Subscript','Superscript','-','RemoveFormat' ] },
         { name: 'colors', items : [ 'TextColor','BGColor' ] },
-        { name: 'links', items : [ 'Link','Unlink' ] },                
-        { name: 'tools', items : [ 'About' ] }
+        { name: 'links', items : [ 'Link','Unlink' ] }                     
     ];
 };
